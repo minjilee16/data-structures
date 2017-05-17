d7548a6b01f53aa640060d3cb950c4f22bf145e2
Hack Reactor Student <student@hackreactor.com>
2017-05-17 16:25:52 -0700

    still on stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let count = 0; 
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    count ++; 
+    storage.count = value; 
   };
 
   someInstance.dequeue = function() {
+    let deque = storage.count;
+    count--; 
+    for (let key in storage ) {
+      if ( key === 0 ) {
+        delete storage.count; 
+      } 
+      key = key - 1;
+    } 
+   
+    return deque; 
   };
 
   someInstance.size = function() {
+    if ( count < 0 ) {
+      return 0; 
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,57 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // debugger;
+    count ++;
+    console.log(count);
+    storage.count = value;
+    console.log(storage.count);
+    //not being pushed to the correct key
+    
   };
 
   someInstance.pop = function() {
+    
+    let popped = storage.count;
+    
+    console.log(storage.count);
+    delete storage.count;
+    count--;
+    return popped;
   };
 
   someInstance.size = function() {
+    if ( count < 0 ) {
+      return 0; 
+    }
+    return count; 
   };
 
   return someInstance;
 };
+
+var stack = Stack();
+stack.push('a');
+// { 0: a }
+stack.push('b');
+stack.push('c');
+//stack.push('d');
+// console.log(stack.storage)
+// stack.pop();
+// stack.pop();
+
+  //push: 
+  //make a new key in storage
+  //set it equal to the passed value
+  //  push that into the storage variable
+  //  increment count variable
+  //pop:
+  //remove the last element in the object
+  //decrement the count 
+  //returns the element popped. 
+  //size: 
+  //
