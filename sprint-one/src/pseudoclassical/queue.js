5a320b543ab646bccc797522f71da4d0e55bc8db
Minji <minjilee8816@gmail.com>
2017-05-18 00:06:08 -0700

    fixed the last error, replacing 'if'

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,34 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  // someInstance  = {};
-  this.count = 0;
   this.storage = {};
-  // return Queue;
+  this.count = 0; 
 };
 
 
 Queue.prototype.enqueue = function (value) {
-  this.count++;
+  this.count ++; 
   this.storage[this.count] = value; 
-};
+}
 
 Queue.prototype.dequeue = function () {
-  while ( this.count > 0 ) {
+  if ( this.count > 0 ) {
     var dequeued = this.storage[1];
-    delete this.storage[1]; 
-    for ( var key in this.storage ) {
+    delete dequeued;
+    for(var key in this.storage ) {
       key--;
-      this.storage[key] = this.storage[key + 1];
+      this.storage[key] = this.storage[key+1];
     }
-    this.count--;
-    return dequeued;     
+    this.count--; 
+    return dequeued; 
   }
-};
+}
+
 
 Queue.prototype.size = function () {
   return this.count;
-};
-
-
-
+}
